f0525db59094d0365ab60a613089d69ea1b3e503
Bernd.Rederlechner@t-systems.com <bernd.rederlechner@t-systems.com>
2024-01-09 09:38:23 +0000

    Fix venv path

--- .devcontainer/pyvenv.sh
+++ .devcontainer/pyvenv.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 WORKDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" >/dev/null 2>&1 && pwd )"
 
 if [ ! -d "${WORKDIR}/.venv" ]; then
     python3 -m venv ${WORKDIR}/.venv
     source ${WORKDIR}/.venv/bin/activate
-    ${WORKDIR}/.venv/bin/pip3 install -r ${WORKDIR}/mtlsgateway/mbedtls/requirements.txt
+    ${WORKDIR}/.venv/bin/pip3 install -r ${WORKDIR}/mtlsgateway/mbedtls/scripts/basic.requirements.txt
 fi
 
 # usually, the venv is activated by VSCode via customisation settings
